86054de91bc2a676d88fe38b171b13a203dc4d96
doraeric <benson.doraemon@gmail.com>
2017-09-15 14:23:13 +0800

    finish q1

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,3 +1,23 @@
 import sys
-infile = sys.argv[1]
-print(infile)
+wordlist = []
+with open(sys.argv[1], "r") as infile:
+    for line in infile:
+        for i in line.split():
+            wordlist.append(i)
+
+out = []
+for word in wordlist:
+    for obj in out:
+        if obj['word'] == word:
+            obj['count'] += 1
+            break
+    else:
+        out.append({'word': word, 'count': 1})
+
+with open("Q1.txt", "w") as fout:
+    for idx, obj in enumerate(out):
+        if idx != len(out):
+            print("%s %d %d" % (obj['word'], idx, obj['count']), file=fout)
+        else:
+            # Seems not necessary
+            print("%s %d %d" % (obj['word'], idx, obj['count']), file=fout, end='')
