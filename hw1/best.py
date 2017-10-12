b9a90ff1db2de97d04493bae3f39bc39115e7186
doraeric <benson.doraemon@gmail.com>
2017-10-12 19:51:24 +0800

    reparse data, rewrite gradient descent, add nr variable, float output

--- hw1/best.py
+++ hw1/best.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from pandas import read_csv
 import sys
+import os
 import numpy as np
 
 try:
@@ -17,17 +18,22 @@ for i in range(0, len(raw), 18):
             if j == 0 and k == 0:
                 x_data.append(['1'])
             x_data[i//18].extend([raw[i+k][j+2]])
-x_data= [[float(j.replace('NR', '0')) for j in i] for i in x_data]
-for i in x_data:
-    i.extend(np.array(i)[1:] ** 2)
+
+try:
+    model_nr = os.environ['model_nr']
+except:
+    model_nr = '0'
+
+x_data= [[float(j.replace('NR', model_nr)) for j in i] for i in x_data]
+for i in range(len(x_data)):
+    x_data[i] = np.array(x_data[i])
+    x_data[i] = np.append(x_data[i], x_data[i][1:]**2)
 x_data = np.array(x_data)
 
 w = np.load(sys.argv[3])
-print(x_data.shape)
-print(w.shape)
-y = [np.matmul(x, w).sum() for x in x_data]
+y = np.matmul(x_data, w).flatten()
 
 f = open(sys.argv[2], 'w')
 print("id,value", file=f)
 for i in range(len(y)):
-    print("%s,%d" % (raw[i*18][0], y[i]), file=f)
+    print("%s,%f" % (raw[i*18][0], y[i]), file=f)
--- hw1/train.py
+++ hw1/train.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from pandas import read_csv
 import numpy as np
+from numpy.linalg import inv
 import random
+import math
+import os
 
 pm25 = 'input_data/train.csv'
 class Dataset:
@@ -23,44 +26,58 @@ lr = 1
 iteration = 1000
 
 # Custom learning rate
-lr_b = 0
 lr_w = None
 
-def close_form_sol():
-    global w, lr, iteration, b_history, w_history, lr_b, lr_w
-    from numpy.linalg import inv
+def closed_form_sol():
+    global w, lr, iteration, w_history, lr_w
     x = train.inputs
     y = train.labels
     w = np.matmul(np.matmul(inv(np.matmul(x.transpose(), x)), x.transpose()), y)
 
 # Iterations
 def Grad_Des():
-    global b, w, lr, iteration, b_history, w_history, lr_b, lr_w
-    import sys
-    for i in range(iteration):
-        if i % (iteration // 10) == 0:
-            print("\b\b\b\b%3d%%" % (i*100/iteration), end='')
-            sys.stdout.flush()
-        if i == iteration-1:
-            print("\b\b\b\b\033[K", end='')
-            sys.stdout.flush()
+    global w, lr, iteration, w_history, lr_w
+    l_rate = 10
+    repeat = 10000
+    x = train.inputs
+    y = train.labels.flatten()
+    x_t = x.transpose()
+    s_gra = np.zeros(len(x[0]))
 
-        b_grad = 0.0
-        w_grad = np.zeros(train.inputs.shape[1])
-        # f(x) = b + w1x1 + w2x2 + Lambda(w1^2 + w2^2)
-        # Loss = variance + lambda wi^2
-        for n in range(train.labels.shape[0]):
-            yn = train.labels[n][0]
-            Loss_deri = 2.0 * (yn - b - np.matmul(train.inputs, w).sum())
-            b_grad = b_grad - Loss_deri
-            w_grad = [w_grad[k] - Loss_deri * train.inputs[n][k] + 2*Lambda*w[k] for k in range(len(w))]
-            # w_grad = [w_grad_n - Loss_deri * xn for w_grad_n, xn in zip(w_grad, train.inputs[n])]
-
-        lr_b = lr_b + b_grad ** 2
-        lr_w = [lr_w_n + w_grad_n ** 2 for lr_w_n, w_grad_n in zip(lr_w, w_grad)]
-        # Update parameters.
-        b = b - lr/np.sqrt(lr_b) * b_grad
-        w = [w[n] - lr/np.sqrt(lr_w[n]) * w_grad[n] for n in range(train.inputs.shape[1])]
+    for i in range(repeat):
+        hypo = np.dot(x,w)
+        loss = hypo - y
+        cost = np.sum(loss**2) / len(x)
+        cost_a  = math.sqrt(cost)
+        gra = np.dot(x_t,loss)
+        s_gra += gra**2
+        ada = np.sqrt(s_gra)
+        w = w - l_rate * gra/ada
+        print ('\riteration: %d | Cost: %f  ' % ( i,cost_a), end='')
+    print('')
+#     for i in range(iteration):
+#         if i % (iteration // 10) == 0:
+#             print("\b\b\b\b%3d%%" % (i*100/iteration), end='')
+#             sys.stdout.flush()
+#         if i == iteration-1:
+#             print("\b\b\b\b\033[K", end='')
+#             sys.stdout.flush()
+#
+#         w_grad = np.zeros(train.inputs.shape[1])
+#         # f(x) = b + w1x1 + w2x2 + Lambda(w1^2 + w2^2)
+#         # Loss = variance + lambda wi^2
+#         for n in range(train.labels.shape[0]):
+#             yn = train.labels[n][0]
+#             Loss_deri = 2.0 * (yn - np.matmul(train.inputs, w).sum())
+#             # b_grad = b_grad - Loss_deri
+#             w_grad = [w_grad[k] - Loss_deri * train.inputs[n][k] + 2*Lambda*w[k] for k in range(len(w))]
+#             # w_grad = [w_grad_n - Loss_deri * xn for w_grad_n, xn in zip(w_grad, train.inputs[n])]
+#
+#         # lr_b = lr_b + b_grad ** 2
+#         lr_w = [lr_w_n + w_grad_n ** 2 for lr_w_n, w_grad_n in zip(lr_w, w_grad)]
+#         # Update parameters.
+#         # b = b - lr/np.sqrt(lr_b) * b_grad
+#         w = [w[n] - lr/np.sqrt(lr_w[n]) * w_grad[n] for n in range(train.inputs.shape[1])]
 
 def calc_error(dataset):
     global w
@@ -93,7 +110,7 @@ def main():
     if s.lower() == 'y':
         s = input('file name: ')
         try:
-            w = np.load(s)['w']
+            w = np.load(s)
         except FileNotFoundError:
             print('File Not Found, ignore')
 
@@ -102,41 +119,46 @@ def main():
     except UnicodeDecodeError:
         df = read_csv(pm25, encoding='utf8')
 
-    raw = df.values.tolist()
-    x_data = []
-    for i in range(0, len(raw), 18):
-        for j in range(24):
-            x_data.append([raw[i][j+3]])
-            for k in range(1, 18):
-                x_data[i//18*24+j].extend([raw[i + k][j+3]])
+    try:
+        model_nr = os.environ['model_nr']
+    except:
+        model_nr = '0'
 
-    x_data_byday = [[float(j.replace('NR', '0')) for j in i] for i in x_data]
+    raw = df.values.tolist()
+    raw = [i[3:] for i in raw]
+    raw = [[float(j.replace('NR', model_nr)) for j in i] for i in raw]
+    raw = np.array(raw)
+    # x[month][hr][feature]
+    x_m_hr_f = []
+    for i in range(12):
+        x_m_hr_f.append(raw[i*18*20:i*18*20+18,:].T)
+        for j in range(1, 20):
+            toadd = raw[i*18*20+j*18:i*18*20+(j+1)*18,:].T
+            x_m_hr_f[-1] = np.concatenate((x_m_hr_f[-1], toadd))
+
     x_data = []
     y_data = []
-    for i in range(len(x_data_byday) - hour):
-        y_data.append([x_data_byday[i+hour][9]])
-        # x_data
-        # [[1, hr0 temp, hr0 ch4, ... , hr1 ... , hr0 temp**2, hr0 ch4**2, ...], [next data...]]
-        #   ^ bias                                ^^^ this may not exist with lower power
-        x_data.append([1])
-        if onlypm25:
-            for j in range(0, hour):
-                x_data[i].extend([x_data_byday[i+j][9]])
+    for month in (range(12)):
+        x_hr_f = x_m_hr_f[month]
+        for i in range(x_hr_f.shape[0] - hour):
+            y_data.append([x_hr_f[i+hour][9]])
+            # x_data
+            # [[1, hr0 temp, hr0 ch4, ... , hr1 ... , hr0 temp**2, hr0 ch4**2, ...], [next data...]]
+            #   ^ bias                                ^^^ this may not exist with lower power
+            x_data.append([1])
+            if onlypm25:
+                toadd = x_hr_f[i:i+hour,9].flatten()
+            else:
+                toadd = x_hr_f[i:i+hour,:].flatten()
+            x_data[-1] = np.append(x_data[-1], toadd)
             if power == 2:
-                for j in range(0, hour):
-                    x_data[i].extend([x_data_byday[i+j][9]**2])
-        else:
-            for j in range(0, hour):
-                x_data[i].extend(x_data_byday[i+j])
-            if power == 2:
-                for j in range(0, hour):
-                    x_data[i].extend(np.square(x_data_byday[i+j]))
+                x_data[-1] = np.append(x_data[-1], toadd**2)
 
-    combined = list(zip(x_data, y_data))
-    random.shuffle(combined)
-    x_data[:], y_data[:] = zip(*combined)
     x_data = np.array(x_data)
     y_data = np.array(y_data)
+    permutation = np.random.permutation(x_data.shape[0])
+    x_data = x_data[permutation]
+    y_data = y_data[permutation]
     if train_all:
         train = Dataset(x_data, y_data)
     else:
@@ -151,7 +173,7 @@ def main():
     if test:
         print("test  dataset: %.2f" % calc_error(test))
     # Grad_Des()
-    close_form_sol()
+    closed_form_sol()
     print("=== Error after training ===")
     print("train dataset: %.2f" % calc_error(train))
     if test:
@@ -166,6 +188,7 @@ def main():
         name += ['-70', '-100'][train_all]
         name += "-%dhr" % hour
         name += "-x%d" % power
+        name += '-nr' + model_nr
         name  = name + '-' + s if s else name
         name += '.npy'
         np.save(name, w)
