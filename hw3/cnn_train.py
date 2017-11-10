2d33e2de15558b51f0a3e98f9a64df426cd783fc
doraeric <benson.doraemon@gmail.com>
2017-11-10 14:07:43 +0800

    add some models

--- hw3/cnn_train.py
+++ hw3/cnn_train.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import ZeroPadding2D, BatchNormalization
 from keras.optimizers import Adam
 
 class DataSet:
@@ -19,7 +20,11 @@ def load_data():
     data.labels = npz['labels']
     return data
 
-def train_model(dataSet):
+def default_train(dataSet, optimizer = Adam()):
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.fit(dataSet.inputs, dataSet.labels, batch_size=100, epochs=20, validation_split=0.3)
+
+def my_model(dataSet):
     # CNN
     model = Sequential()
     model.add(Conv2D(filters=25, kernel_size=(3, 3), input_shape=(48, 48, 1)))
@@ -37,31 +42,84 @@ def train_model(dataSet):
     model.add(Dense(300, activation='relu'))
     model.add(Dropout(0.3))
     model.add(Dense(7, activation='softmax'))
+    return model
+
+def VGG16():
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(48, 48, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+def AlexNet():
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3), input_shape=(48, 48, 1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2))) # 23
+    model.add(Conv2D(128, (2, 2)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2))) # 11
+    model.add(Conv2D(192, (2, 2), activation='relu'))
+    model.add(Conv2D(192, (2, 2), activation='relu'))
+    model.add(Conv2D(192, (2, 2))) # 7
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.3))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(7))
+    model.add(BatchNormalization())
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-    model.fit(dataSet.inputs, dataSet.labels, batch_size=100, epochs=20, validation_split=0.3)
-#     # CNN
-#     model = Sequential()
-#     model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(48, 48, 1)))
-#     model.add(Conv2D(filters=32, kernel_size=(3, 3)))
-#     model.add(MaxPooling2D(pool_size=(2, 2)))
-#     model.add(Dropout(0.1))
-#     model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(48, 48, 1)))
-#     model.add(Conv2D(filters=64, kernel_size=(3, 3)))
-#     model.add(MaxPooling2D(pool_size=(2, 2)))
-#     model.add(Dropout(0.1))
-#     model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(48, 48, 1)))
-#     model.add(Conv2D(filters=128, kernel_size=(3, 3)))
-#     model.add(MaxPooling2D(pool_size=(2, 2)))
-#     model.add(Dropout(0.4))
-#     model.add(Flatten())
-#     # Fully connected
-#     model.add(Dense(2048, activation='relu'))
-#     model.add(Dropout(0.5))
-#     model.add(Dense(7, activation='softmax'))
-#     model.add(Activation('softmax'))
-#     model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-#     model.fit(dataSet.inputs, dataSet.labels, batch_size=100, epochs=40, validation_split=0.3)
     return model
 
 def main():
