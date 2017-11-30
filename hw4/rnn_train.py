5c70a16a3daad95cad3c3d3455d0fa983f0009d6
doraeric <benson.doraemon@gmail.com>
2017-12-01 03:12:04 +0800

    add parser and save formatted data

--- hw4/rnn_train.py
+++ hw4/rnn_train.py
@@ -5,6 +5,8 @@ from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
 import numpy as np
+import argparse
+import pickle
 import utils as u
 
 train_label_x = 'input_data/trimmed_inputs.txt'
@@ -26,19 +28,33 @@ def collect(model, d):
     return newdata
 
 def main():
-    print('Loading data...')
-    d1, wdict = u.load_data(train_label_x, train_label_y, num_words=10000)
-    d2, wdict = u.load_data(train_nolabel, wdict=wdict)
-    d1_valid = d1.split(0.3)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load-pickle", action='store_true', dest='load_pkl')
+    parser.add_argument("--save-pickle", action='store_true', dest='save_pkl')
+    parser.add_argument("--smaller", action='store_true')
+    args = parser.parse_args()
 
-    d1.shrink(0.001)
-    d1_valid.shrink(0.001)
-    d2.shrink(0.001)
+    print('Loading data...')
+    if not args.load_pkl:
+        d1, wdict = u.load_data(train_label_x, train_label_y, num_words=10000)
+        d2, wdict = u.load_data(train_nolabel, wdict=wdict)
+        d1_valid = d1.split(0.3)
+        d1.inputs = sequence.pad_sequences(d1.inputs, maxlen=maxlen)
+        d1_valid.inputs = sequence.pad_sequences(d1_valid.inputs, maxlen=maxlen)
+        d2.inputs = sequence.pad_sequences(d2.inputs, maxlen=maxlen)
+        if args.save_pkl:
+            with open('input_data/quick.pkl', 'wb') as f:
+                pickle.dump((d1, d1_valid, d2), f, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        with open('input_data/quick.pkl', 'rb') as f:
+            d1, d1_valid, d2 = pickle.load(f)
+
+    if args.smaller:
+        d1.shrink(0.001)
+        d1_valid.shrink(0.001)
+        d2.shrink(0.001)
 
     print('Pad sequences (samples x time)')
-    d1.inputs = sequence.pad_sequences(d1.inputs, maxlen=maxlen)
-    d1_valid.inputs = sequence.pad_sequences(d1_valid.inputs, maxlen=maxlen)
-    d2.inputs = sequence.pad_sequences(d2.inputs, maxlen=maxlen)
     print('x_train shape: %s' % str(d1.inputs.shape))
     print('x_test  shape: %s' % str(d1_valid.inputs.shape))
 
@@ -60,7 +76,7 @@ def main():
     # reinforce
     step = 5
     for i in range(step):
-        print("current step: %d" % step)
+        print("current step: %d" % i)
         newdata = collect(model, d2)
         d1.join(newdata)
         model.fit(d1.inputs, d1.labels,
@@ -72,8 +88,18 @@ def main():
     print('Test score: %f'% score)
     print('Test accuracy: %f'% acc)
 
-    test, wdict = u.load_data('input_data/trimmed_test.txt', wdict=wdict)
-    test.inputs = sequence.pad_sequences(test.inputs, maxlen=maxlen)
+    if not args.load_pkl:
+        test, wdict = u.load_data('input_data/trimmed_test.txt', wdict=wdict)
+        test.inputs = sequence.pad_sequences(test.inputs, maxlen=maxlen)
+        if args.save_pkl:
+            with open('input_data/test.pkl', 'wb') as f:
+                pickle.dump(test, f, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        with open('input_data/test.pkl', 'rb') as f:
+            test = pickle.load(f)
+
+    print('test shape: %s' % str(test.inputs.shape))
+    print('Predicting...')
     prob = np.rint(model.predict(test.inputs)).flatten()
     with open('predict.txt', 'w') as f:
         print('id,label', file=f)
