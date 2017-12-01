bf437fa3903ea7cc9f5fa77c27649cb40f3d5b1e
doraeric <benson.doraemon@gmail.com>
2017-12-01 19:06:02 +0800

    fix script

--- hw4/trim_label.sh
+++ hw4/trim_label.sh
@@ -8,7 +8,7 @@ cp $1 input_data/trimmed_inputs.txt
 sed -i 's/^[01] +++$+++ //' input_data/trimmed_inputs.txt
 cut -d ' ' -f1 $1 > input_data/trimmed_labels.txt
 
-if [ $# -lt 2] ; then
+if [ $# -lt 2 ] ; then
 	exit 0
 fi
 cp $2 input_data/trimmed_test.txt
