d27326caeb8a0dd41d5a494dd19245b4abc1b039
doraeric <benson.doraemon@gmail.com>
2017-12-07 13:02:31 +0800

    add appendable history callback

---
+++ hw4/utils/keras.py
@@ -0,0 +1,20 @@
+from keras.callbacks import Callback
+
+class History(Callback):
+    """Callback that records events into a `History` object.
+    This callback is automatically applied to
+    every Keras model. The `History` object
+    gets returned by the `fit` method of models.
+    """
+
+    def on_train_begin(self, logs=None):
+        if not hasattr(self, 'epoch'):
+            self.epoch = []
+        if not hasattr(self, 'history'):
+            self.history = {}
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        self.epoch.append(epoch)
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
