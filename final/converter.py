f53801e34ddc7733f0893b5cdaf0b04601d39fa4
doraeric <benson.doraemon@gmail.com>
2017-12-19 19:08:35 +0800

    fix parse error

--- final/converter.py
+++ final/converter.py
@@ -4,12 +4,23 @@
 from sys import argv
 import numpy as np
 
-if len(argv) < 3:
-    print('./converter.py <fasttext-out> <emb.npy>')
+if len(argv) < 4:
+    print('./converter.py <caption> <fasttext-out> <emb.npy>')
     exit(-1)
 
-ctx = open(argv[1]).read()
+ctx = open(argv[2]).read()
 lines = ctx.rstrip().split('\n')
 lines = [ line.rstrip().split()[1:] for line in lines ]
 lines = np.array(lines).astype(float)
-np.save(argv[2], lines)
+
+cap = open(argv[1]).read()
+cap_lines = cap.rstrip().split('\n')
+cnt = [len(line.rstrip().split()) for line in cap_lines]
+
+s = 0
+lines_words = []
+for c in cnt:
+    lines_words.append(lines[s:s+c,:])
+    s += c
+
+np.save(argv[3], lines_words)
