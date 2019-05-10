2fb9b5192fa25f81bdc725a5ebdaf17c46d094aa
doraeric <benson.doraemon@gmail.com>
2019-05-10 18:54:20 +0800

    better argparse

--- hw4/rnn_train.py
+++ hw4/rnn_train.py
@@ -17,11 +17,7 @@ train_label_x = 'input_data/trimmed_inputs.txt'
 train_label_y = 'input_data/trimmed_labels.txt'
 train_nolabel = 'input_data/training_nolabel.txt'
 
-max_features = 20000
-maxlen = 80  # cut texts after this number of words (among top max_features most common words)
-batch_size = 32
-
-def collect(model, d):
+def collect(model, d, batch_size):
     pred = model.predict(d.inputs, batch_size=batch_size)
     mask = np.logical_or(pred>0.95, pred<0.05).flatten()
     coll = d.inputs[mask]
@@ -31,32 +27,27 @@ def collect(model, d):
     newdata = u.DataSet(coll, pred)
     return newdata
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name')
-    parser.add_argument("--load-pickle", action='store_true', dest='load_pkl')
-    parser.add_argument("--save-pickle", action='store_true', dest='save_pkl')
-    parser.add_argument("--smaller", action='store_true')
-    args = parser.parse_args()
-
+def main(model_name: str='model', load_pkl: bool=False, save_pkl: bool=False,
+        fast: bool=False, num_words: int=20000, maxlen: int=80, batch_size: int=32,
+        reinforce_step: int=0, **kwargs):
     print('Loading data...')
-    if not args.load_pkl:
-        d1, wdict = u.load_data(train_label_x, train_label_y, num_words=20000)
-        with open(args.name + '_wdict.pkl', 'wb') as f:
+    if not load_pkl:
+        d1, wdict = u.load_data(train_label_x, train_label_y, num_words=num_words)
+        with open(model_name + '_wdict.pkl', 'wb') as f:
             pickle.dump(wdict, f)
         d2, wdict = u.load_data(train_nolabel, wdict=wdict)
         d1_valid = d1.split(0.2)
         d1.inputs = sequence.pad_sequences(d1.inputs, maxlen=maxlen)
         d1_valid.inputs = sequence.pad_sequences(d1_valid.inputs, maxlen=maxlen)
         d2.inputs = sequence.pad_sequences(d2.inputs, maxlen=maxlen)
-        if args.save_pkl:
+        if save_pkl:
             with open('input_data/quick.pkl', 'wb') as f:
                 pickle.dump((d1, d1_valid, d2), f, protocol=pickle.HIGHEST_PROTOCOL)
     else:
         with open('input_data/quick.pkl', 'rb') as f:
             d1, d1_valid, d2 = pickle.load(f)
 
-    if args.smaller:
+    if fast:
         d1.shrink(0.001)
         d1_valid.shrink(0.001)
         d2.shrink(0.001)
@@ -66,7 +57,7 @@ def main():
     print('x_test  shape: %s' % str(d1_valid.inputs.shape))
 
     model = Sequential()
-    model.add(Embedding(max_features, 128))
+    model.add(Embedding(num_words, 128))
     model.add(LSTM(512, dropout=0.2, recurrent_dropout=0.2))
     model.add(Dense(256))
     model.add(Dropout(0.2))
@@ -78,7 +69,7 @@ def main():
                   metrics=['accuracy'])
 
     earlystopping = EarlyStopping(monitor='val_acc', patience=5)
-    checkpointer = ModelCheckpoint(filepath=args.name+'_weight.h5', monitor='val_acc', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath=model_name+'_weight.h5', monitor='val_acc', verbose=1, save_best_only=True)
 
     model.fit(d1.inputs, d1.labels,
               batch_size=batch_size,
@@ -87,10 +78,9 @@ def main():
               callbacks=[earlystopping, checkpointer])
 
     # reinforce
-    step = 5
-    for i in range(step):
+    for i in range(reinforce_step):
         print("current step: %d" % i)
-        newdata = collect(model, d2)
+        newdata = collect(model, d2, batch_size)
         d1.join(newdata)
         model.fit(d1.inputs, d1.labels,
                   batch_size=batch_size,
@@ -102,10 +92,10 @@ def main():
     print('Valid loss: %f'% score)
     print('Valid accuracy: %f'% acc)
 
-    if not args.load_pkl:
+    if not load_pkl:
         test, wdict = u.load_data('input_data/trimmed_test.txt', wdict=wdict)
         test.inputs = sequence.pad_sequences(test.inputs, maxlen=maxlen)
-        if args.save_pkl:
+        if save_pkl:
             with open('input_data/test.pkl', 'wb') as f:
                 pickle.dump(test, f, protocol=pickle.HIGHEST_PROTOCOL)
     else:
@@ -114,13 +104,24 @@ def main():
 
     print('test shape: %s' % str(test.inputs.shape))
     print('Predicting...')
-    model = load_model(args.name+'_weight.h5')
+    model = load_model(model_name+'_weight.h5')
     prob = np.rint(model.predict(test.inputs)).flatten()
-    with open(args.name+'_predict.txt', 'w') as f:
+    with open(model_name+'_predict.txt', 'w') as f:
         print('id,label', file=f)
         for i in range(prob.shape[0]):
             print("%d,%.0f" % (i, prob[i]), file=f)
     return model
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    parser.add_argument('model_name')
+    parser.add_argument("--load-pickle", action='store_true', dest='load_pkl')
+    parser.add_argument("--save-pickle", action='store_true', dest='save_pkl')
+    parser.add_argument("--fast", action='store_true',
+            help="Enable fast mode to load partial data only")
+    parser.add_argument('--max-features', type=int)
+    parser.add_argument('--maxlen', type=int, help='cut texts after this number of words (among top num_words most common words)')
+    parser.add_argument('--batch-size', type=int)
+    parser.add_argument('--reinforce_step', type=int)
+    args = vars(parser.parse_args())
+    main(**args)
